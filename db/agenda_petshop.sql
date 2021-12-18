8ef0f64cf137021ddc866b842d78986132a95335
Rui Nascimento <sruy19@gmail.com>
2021-12-18 08:02:20 -0300

    sql database

---
+++ db/agenda_petshop.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE agenda_petshop;
+
+USE agenda_petshop;
+
+CREATE TABLE atendimentos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente VARCHAR(50) NOT NULL,
+    pet VARCHAR(20),
+    servico VARCHAR(20) NOT NULL,
+    observacoes TEXT 
+);
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const customExpress = require('./config/customExpress');
 const app = customExpress();
 const conexao = require('./infraestrutura/conexao');
-const Tabelas = require('./infraestrutura/tabelas');
+//const Tabelas = require('./infraestrutura/tabelas');
 
 conexao.connect(erro => {
     if (erro) {
         console.log(erro);
     } else {
         console.log('✔️ MySQL');
-        const tabela = new Tabelas();
         app.listen(process.env.PORT, ()=> {
             console.log(`🚀 Server is run ${process.env.HOST}:${process.env.PORT}`)
         })
